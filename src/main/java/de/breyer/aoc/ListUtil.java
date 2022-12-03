3ce4daeb33f65b2cc5cac2a909e7b45fd5d2c09a
Mario Breyer <mario.breyer@cologne-intelligence.de>
2022-12-03 10:47:53 +0100

    solved 2022 03

---
+++ src/main/java/de/breyer/aoc/ListUtil.java
@@ -0,0 +1,13 @@
+package de.breyer.aoc;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class ListUtil {
+
+    public static <T> Stream<List<T>> subGroups(List<T> list, int size) {
+        return IntStream.range(0, list.size() / size).mapToObj(start -> list.subList(3 * start, 3 * start + size));
+    }
+
+}
---
+++ src/main/java/de/breyer/aoc/y2022/D03.java
@@ -0,0 +1,53 @@
+package de.breyer.aoc.y2022;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import de.breyer.aoc.AbstractAocPuzzle;
+import de.breyer.aoc.AocPuzzle;
+import de.breyer.aoc.ListUtil;
+
+@AocPuzzle("2022_03")
+public class D03 extends AbstractAocPuzzle {
+
+    @Override
+    protected void partOne() {
+        int sumPriority = lines.stream().map(this::splitInHalf).map(this::calcPriority).reduce(0, Integer::sum);
+        System.out.println("sum of priorities: " + sumPriority);
+    }
+
+    private String[] splitInHalf(String line) {
+        final int mid = line.length() / 2;
+        return new String[]{line.substring(0, mid), line.substring(mid)};
+    }
+
+    private int calcPriority(String[] halves) {
+        Set<Character> wrongCharacters = new HashSet<>();
+        for (char character : halves[0].toCharArray()) {
+            if (-1 != halves[1].indexOf(character)) {
+                wrongCharacters.add(character);
+            }
+        }
+        return wrongCharacters.stream().map(this::getPriority).reduce(0, Integer::sum);
+    }
+
+    private int getPriority(char character) {
+        return Character.isUpperCase(character) ? Character.toLowerCase(character) - 70 : Character.toUpperCase(character) - 64;
+    }
+
+    @Override
+    protected void partTwo() {
+        int sumPriority = ListUtil.subGroups(lines, 3).map(this::findBadgePriority).reduce(0, Integer::sum);
+        System.out.println("sum of badge priorities: " + sumPriority);
+    }
+
+    private int findBadgePriority(List<String> subGroup) {
+        for (char character : subGroup.get(0).toCharArray()) {
+            if (-1 != subGroup.get(1).indexOf(character) && -1 != subGroup.get(2).indexOf(character)) {
+                return getPriority(character);
+            }
+        }
+        return 0;
+    }
+
+}
