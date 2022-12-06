2241f050b291a0e06907484bd02e594fec6ed2c4
Mario Breyer <mario.breyer@cologne-intelligence.de>
2022-12-06 07:38:59 +0100

    solved 2022 06

---
+++ src/main/java/de/breyer/aoc/y2022/D06.java
@@ -0,0 +1,40 @@
+package de.breyer.aoc.y2022;
+
+import java.util.HashSet;
+import java.util.Set;
+import de.breyer.aoc.AbstractAocPuzzle;
+import de.breyer.aoc.AocPuzzle;
+
+@AocPuzzle("2022_06")
+public class D06 extends AbstractAocPuzzle {
+
+    @Override
+    protected void partOne() {
+        int processedChars = findPacketMarker(4);
+        System.out.println("chars process before first start-of-packet marker: " + processedChars);
+    }
+
+    private int findPacketMarker(int distinctCharCount) {
+        String line = lines.get(0);
+        for (int i = distinctCharCount - 1; i <= line.length(); i++) {
+            Set<Character> distinctChars = new HashSet<>();
+
+            for (int j = 0; j < distinctCharCount; j++) {
+                distinctChars.add(line.charAt(i - j));
+            }
+
+            if (distinctChars.size() == distinctCharCount) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    protected void partTwo() {
+        int processedChars = findPacketMarker(14);
+        System.out.println("chars process before first start-of-packet marker: " + processedChars);
+    }
+
+}
