0fde617bd835160c7c134c2f1e8c03b8ad457837
Mario Breyer <mario.breyer@cologne-intelligence.de>
2022-12-09 18:36:09 +0100

    solved 9 2022

---
+++ src/main/java/de/breyer/aoc/y2022/D09.java
@@ -0,0 +1,80 @@
+package de.breyer.aoc.y2022;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import de.breyer.aoc.AbstractAocPuzzle;
+import de.breyer.aoc.AocPuzzle;
+import de.breyer.aoc.data.Point2D;
+
+@AocPuzzle("2022_09")
+public class D09 extends AbstractAocPuzzle {
+
+    @Override
+    protected void partOne() {
+        int visited = simulate(2);
+        System.out.println("tail visited " + visited + " positions");
+    }
+
+    private int simulate(int size) {
+        Set<Point2D> visitedByTail = new HashSet<>();
+        List<Point2D> rope = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            rope.add(new Point2D(0, 0));
+        }
+        visitedByTail.add(rope.get(rope.size() - 1));
+
+        for (String line : lines) {
+            String[] split = line.split(" ");
+            int moves = Integer.parseInt(split[1]);
+            Direction direction = Direction.valueOf(split[0].charAt(0));
+
+            for (int i = 0; i < moves; i++) {
+                Point2D newPosition = moveFirstKnot(rope.get(0), direction);
+                rope.remove(0);
+                rope.add(0, newPosition);
+
+                for (int j = 1; j < rope.size(); j++) {
+                    if (needKnotToMove(rope.get(j - 1), rope.get(j))) {
+                        newPosition = moveKnot(rope.get(j - 1), rope.get(j));
+                        rope.remove(j);
+                        rope.add(j, newPosition);
+
+                        if (j == rope.size() - 1) {
+                            visitedByTail.add(newPosition);
+                        }
+                    }
+                }
+            }
+        }
+
+        return visitedByTail.size();
+    }
+
+    private Point2D moveFirstKnot(Point2D knot, Direction direction) {
+        int x = direction.getXExpression().apply(knot.getX(), 1);
+        int y = direction.getYExpression().apply(knot.getY(), 1);
+        return new Point2D(x, y);
+    }
+
+    private boolean needKnotToMove(Point2D previous, Point2D current) {
+        int xDiff = Math.abs(previous.getX() - current.getX());
+        int yDiff = Math.abs(previous.getY() - current.getY());
+
+        return xDiff > 1 || yDiff > 1;
+    }
+
+    private Point2D moveKnot(Point2D previous, Point2D current) {
+        int xDiff = Integer.compare(previous.getX(), current.getX());
+        int yDiff = Integer.compare(previous.getY(), current.getY());
+        return new Point2D(current.getX() + xDiff, current.getY() + yDiff);
+    }
+
+    @Override
+    protected void partTwo() {
+        int visited = simulate(10);
+        System.out.println("tail visited " + visited + " positions");
+    }
+
+}
---
+++ src/main/java/de/breyer/aoc/y2022/Direction.java
@@ -0,0 +1,37 @@
+package de.breyer.aoc.y2022;
+
+import java.util.function.BiFunction;
+
+public enum Direction {
+
+    UP((x, i) -> x, (y, i) -> y - i),
+    RIGHT((x, i) -> x + 1, (y, i) -> y),
+    DOWN((x, i) -> x, (y, i) -> y + i),
+    LEFT((x, i) -> x - 1, (y, i) -> y);
+
+    private final BiFunction<Integer, Integer, Integer> xExpression;
+    private final BiFunction<Integer, Integer, Integer> yExpression;
+
+    Direction(BiFunction<Integer, Integer, Integer> xExpression, BiFunction<Integer, Integer, Integer> yExpression) {
+        this.xExpression = xExpression;
+        this.yExpression = yExpression;
+    }
+
+    public static Direction valueOf(char character) {
+        return switch (character) {
+            case 'U' -> UP;
+            case 'D' -> DOWN;
+            case 'L' -> LEFT;
+            case 'R' -> RIGHT;
+            default -> throw new IllegalStateException("Unexpected value: " + character);
+        };
+    }
+
+    public BiFunction<Integer, Integer, Integer> getXExpression() {
+        return xExpression;
+    }
+
+    public BiFunction<Integer, Integer, Integer> getYExpression() {
+        return yExpression;
+    }
+}
