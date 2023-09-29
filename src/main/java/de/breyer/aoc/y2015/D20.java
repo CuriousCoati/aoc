d198fb75811f64fc245ebbc4d0fb5b72272239f0
Mario Breyer <mario.breyer@cologne-intelligence.de>
2023-09-29 18:50:22 +0200

    solved 20 2015

---
+++ src/main/java/de/breyer/aoc/y2015/D20.java
@@ -0,0 +1,47 @@
+package de.breyer.aoc.y2015;
+
+import java.util.HashMap;
+import java.util.function.BiPredicate;
+import de.breyer.aoc.AbstractAocPuzzle;
+import de.breyer.aoc.AocPuzzle;
+
+@AocPuzzle("2015_20")
+public class D20 extends AbstractAocPuzzle {
+
+    private static final int TARGET = 34000000;
+
+    @Override
+    protected void partOne() {
+        var lowestHouseNumber = findLowestHouseNumberToReachTwo(10, (i, j) -> i * j <= 786240);
+        System.out.println("lowest house number: " + lowestHouseNumber);
+    }
+
+    @Override
+    protected void partTwo() {
+        var lowestHouseNumber = findLowestHouseNumberToReachTwo(11, (i, j) -> i <= 50);
+        System.out.println("lowest house number: " + lowestHouseNumber);
+    }
+
+    private int findLowestHouseNumberToReachTwo(int presentMultiplier, BiPredicate<Integer, Integer> stopCondition) {
+        var cache = new HashMap<Integer, Integer>();
+        var houseNumber = 0;
+        var presents = 0;
+
+        do {
+            houseNumber++;
+
+            for (int i = 1; stopCondition.test(i, houseNumber); i++) {
+                var curHouseNumber = houseNumber * i;
+                var curPresentsAtHouse = cache.getOrDefault(curHouseNumber, 0);
+                curPresentsAtHouse += houseNumber * presentMultiplier;
+                cache.put(curHouseNumber, curPresentsAtHouse);
+            }
+
+            presents = cache.get(houseNumber);
+            cache.remove(houseNumber);
+        } while (presents < TARGET);
+
+        return houseNumber;
+    }
+
+}
